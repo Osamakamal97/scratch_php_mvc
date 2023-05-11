46a43d050edc317527bd89dad972e968bea3ae3b
Osamakamal97 <osmaka1997@gmail.com>
2023-05-11 14:11:09 +0300

    Implement routing and files structure

--- core/Application.php
+++ core/Application.php
@@ -1,12 +1,28 @@
 <?php
 
+namespace app\core;
+
+/**
+ * Class Application
+ * 
+ * @author Osama Elnabris <osama.elnabris@gmail.com>
+ * @package app\core
+ */
 class Application
 {
 
     public Router $router;
+    public Request $request;
 
     public function __construct()
     {
-        $this->router = new Router();
+        $this->request = new Request();
+        $this->router = new Router($this->request);
     }
+
+    public function run()
+    {
+        $this->router->resolve();
+    }
+
 }
--- core/Router.php
+++ core/Router.php
@@ -1,11 +1,39 @@
 <?php
 
+namespace app\core;
+
+/**
+ * Class Router
+ * 
+ * @author Osama Elnabris <osama.elnabris@gmail.com>
+ * @package app\core
+ */
 class Router
 {
 
-    public function __construct()
-    {
+    public Request $request;
+    protected array $routes = [];
 
+    public function __construct(\app\core\Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback == false) {
+            echo "Not Found";
+            exit;
+        }
+        echo call_user_func($callback);
     }
 
 }
